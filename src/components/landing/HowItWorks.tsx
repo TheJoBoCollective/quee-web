3a500b83408ea8d6ecadf32892ce0ce226b56e95
jolaolu <adebayoanjola@gmail.com>
2025-05-04 00:28:22 +0100

    add HowItWorks component to showcase app functionality

---
+++ src/components/landing/HowItWorks.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HowItWorks = () => {
+  return (
+    <section className="bg-white flex flex-col items-center py-30 px-24 min-h-screen">
+      <h3 className="text-5xl font-medium pb-6">
+        Turn your setlist into a paycheck
+      </h3>
+      <p className="text-xl pb-7 max-w-xl text-center">
+        Get paid to play, manage live song requests, and keep the crowd
+        hyped—all from one app. No more awkward requests, just smooth
+        transactions and real-time control.
+      </p>
+    </section>
+  );
+}
+
+export default HowItWorks
